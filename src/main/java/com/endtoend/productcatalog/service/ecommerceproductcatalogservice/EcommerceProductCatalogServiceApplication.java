3d2a9d5279830f49997d21f88f2c061179e48304
RakeshYedlapalli <rak.mdg94@gmail.com>
2024-09-01 22:17:33 +0800

    integration for e-commerce

---
+++ src/main/java/com/endtoend/productcatalog/service/ecommerceproductcatalogservice/EcommerceProductCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.endtoend.productcatalog.service.ecommerceproductcatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceProductCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceProductCatalogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/endtoend/productcatalog/service/ecommerceproductcatalogservice/EcommerceProductCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endtoend.productcatalog.service.ecommerceproductcatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceProductCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
